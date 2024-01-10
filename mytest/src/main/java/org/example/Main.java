1f87c447697215e46e1aeb23c484613871b22cbc
maiyupeng <2546300629@qq.com>
2024-01-10 09:01:59 +0800

    第二次提交，更新链表测试题

--- mytest/src/main/java/org/example/Main.java
+++ mytest/src/main/java/org/example/Main.java
@@ -8,25 +8,59 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        int[] nums = {-1,-2,-3,-4,-5};
-        main.twoSum(nums, -8);
+        int[] nums = {-1, -2, -3, -4, -5};
+        ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))));
+        ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        ListNode listNode = main.addTwoNumbers(l1, l2);
+
+    }
+
+    public ListNode loop(ListNode l) {
+        while (true) {
+            if (l.next == null) {
+                return l;
+            }
+            l = l.next;
+        }
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int b = 0;
+        while (true) {
+            int i = l1.val + l2.val;
+            if (head == null) { //  第一次循环
+                head = new ListNode(i % 10);
+                b = i / 10;
+            } else {
+                if (tail == null) {
+                    tail = new ListNode(i % 10 + b);
+                    head.next = tail;
+                } else {
+                    tail.next = new ListNode(i % 10 + b);
+                    tail = tail.next;
+                }
+                b = i / 10;
+            }
+            if (l1.next == null && l2.next == null) break;
+            if (l1.next != null) l1 = l1.next;
+            if (l2.next != null) l2 = l2.next;
+        }
+        return head;
     }
 
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            if (target>0? nums[i] <= target:nums[i] >= target) {
-                for (int j = nums.length-1; j >= 0; j--) {
-                    if (nums[i] + nums[j] == target && i != j) {
-                        arr[0] = i;
-                        arr[1] = j;
-                        break;
-                    }
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+            int another = target - nums[i];
+            if (map.containsKey(another)) {
+                return new int[]{i, map.get(another)};
             }
+            map.put(nums[i], i);
         }
-        System.out.println(Arrays.toString(arr));
-        return arr;
+        return null;
     }
 
     //    提供一个数组arr，和要删除的数字数量k,求arr删除k个元素之后数组中最好能剩几种数字？
